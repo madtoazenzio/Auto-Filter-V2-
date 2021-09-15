3e925309f6ed6c10b388078a82dfbcf4dfbf1474
Mad To AzenZio <89691212+madtoazenzio@users.noreply.github.com>
2021-09-15 06:48:52 +0530

    Update motech.py

--- bot/motech.py
+++ bot/motech.py
@@ -1,5 +1,5 @@
 # Update
-MT_BOT_UPDATES = "t.me/Mo_tech_YT"
+MT_BOT_UPDATES = "t.me/illustratecreator"
 # Your Main Channel
-BOT_UPDATES = "t.me/Mo_Tech_YT"
+BOT_UPDATES = "t.me/illustratecreator"
 
